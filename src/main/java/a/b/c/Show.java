101bd0f1d9aa0777752eb75f313f2f9fd1e3248c
J <3cgg@163.com>
2017-12-30 00:19:26 +0800

    -

---
+++ src/main/java/a/b/c/Show.java
@@ -0,0 +1,19 @@
+package a.b.c;
+
+import s.a.b.c.B;
+
+/**
+ * Created by J on 2017/12/29.
+ */
+public class Show {
+
+
+    public static void main(String[] args) {
+
+        B b=new B("J",20);
+        String sss=b.showName();
+        System.out.println(sss);
+    }
+
+
+}
